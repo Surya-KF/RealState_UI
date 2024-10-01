329a1679eb9e3c73d5851ca580b1c28df6bbd7ee
SuryaKF <suryakf04@gmail.com>
2024-10-01 23:31:17 +0530

    Updated the responsiveness of the front page.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,36 @@
-import "./navbar.scss"
-function Navbar(){
-    return(
-        <nav>
-            <div className="left">Left</div>
-            <div className="right">Right</div>
-        </nav>
-    )
- }
+import './navbar.scss';
 
- export default Navbar; 
+function Navbar() {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className='logo'>
+          <img src="/logo.png" alt="" />
+          <span>Real Estate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className='register'>Sign up</a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
